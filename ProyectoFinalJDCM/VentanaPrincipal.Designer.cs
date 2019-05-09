69a9b12ff66de0de0d537ea841c2968ef2578eea
Daniel Hebrero <danielhebrero94@gmail.com>
2019-05-09 16:09:53 +0200

    Añado un dataGriefView que muestre las consultas y optimizo el codigo creando los métodos hazLaConsulta(String query) y limpiaListaConsultas()

--- ProyectoFinalJDCM/VentanaPrincipal.Designer.cs
+++ ProyectoFinalJDCM/VentanaPrincipal.Designer.cs
@@ -28,31 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.desplegableCine = new System.Windows.Forms.ComboBox();
+            this.listaConsultas = new System.Windows.Forms.DataGridView();
+            this.botonPeliculas = new System.Windows.Forms.Button();
+            this.botonDirectores = new System.Windows.Forms.Button();
+            this.botonActores = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.listaConsultas)).BeginInit();
             this.SuspendLayout();
             // 
-            // desplegableCine
+            // listaConsultas
             // 
-            this.desplegableCine.FormattingEnabled = true;
-            this.desplegableCine.Location = new System.Drawing.Point(225, 141);
-            this.desplegableCine.Name = "desplegableCine";
-            this.desplegableCine.Size = new System.Drawing.Size(121, 21);
-            this.desplegableCine.TabIndex = 0;
+            this.listaConsultas.AllowUserToOrderColumns = true;
+            this.listaConsultas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.listaConsultas.Location = new System.Drawing.Point(12, 90);
+            this.listaConsultas.Name = "listaConsultas";
+            this.listaConsultas.ReadOnly = true;
+            this.listaConsultas.Size = new System.Drawing.Size(495, 348);
+            this.listaConsultas.TabIndex = 5;
+            // 
+            // botonPeliculas
+            // 
+            this.botonPeliculas.Location = new System.Drawing.Point(646, 90);
+            this.botonPeliculas.Name = "botonPeliculas";
+            this.botonPeliculas.Size = new System.Drawing.Size(75, 23);
+            this.botonPeliculas.TabIndex = 6;
+            this.botonPeliculas.Text = "Peliculas";
+            this.botonPeliculas.UseVisualStyleBackColor = true;
+            this.botonPeliculas.Click += new System.EventHandler(this.botonPeliculas_Click);
+            // 
+            // botonDirectores
+            // 
+            this.botonDirectores.Location = new System.Drawing.Point(646, 119);
+            this.botonDirectores.Name = "botonDirectores";
+            this.botonDirectores.Size = new System.Drawing.Size(75, 23);
+            this.botonDirectores.TabIndex = 7;
+            this.botonDirectores.Text = "Directores";
+            this.botonDirectores.UseVisualStyleBackColor = true;
+            this.botonDirectores.Click += new System.EventHandler(this.botonDirectores_Click);
+            // 
+            // botonActores
+            // 
+            this.botonActores.Location = new System.Drawing.Point(646, 148);
+            this.botonActores.Name = "botonActores";
+            this.botonActores.Size = new System.Drawing.Size(75, 23);
+            this.botonActores.TabIndex = 8;
+            this.botonActores.Text = "Actores";
+            this.botonActores.UseVisualStyleBackColor = true;
+            this.botonActores.Click += new System.EventHandler(this.botonActores_Click);
             // 
             // VentanaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.desplegableCine);
+            this.Controls.Add(this.botonActores);
+            this.Controls.Add(this.botonDirectores);
+            this.Controls.Add(this.botonPeliculas);
+            this.Controls.Add(this.listaConsultas);
             this.Name = "VentanaPrincipal";
             this.Text = "VentanaPrincipal";
+            ((System.ComponentModel.ISupportInitialize)(this.listaConsultas)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ComboBox desplegableCine;
+        private System.Windows.Forms.DataGridView listaConsultas;
+        private System.Windows.Forms.Button botonPeliculas;
+        private System.Windows.Forms.Button botonDirectores;
+        private System.Windows.Forms.Button botonActores;
     }
 }
--- ProyectoFinalJDCM/VentanaPrincipal.cs
+++ ProyectoFinalJDCM/VentanaPrincipal.cs
@@ -9,14 +9,19 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 
+/*
+ * Autor: Daniel Hebrero Núñez, Amo y señor del código
+ */
+
 namespace ProyectoFinalJDCM
 {
     public partial class VentanaPrincipal : Form
     {
+        private DataTable datos = new DataTable();  //Declaro el DataTable de datos
         public VentanaPrincipal()
         {
             InitializeComponent();
-            rellenaComboAutores();
+            
         }
 
         protected override void OnFormClosed(FormClosedEventArgs e)
@@ -25,20 +30,51 @@ namespace ProyectoFinalJDCM
             Application.Exit();
         }
 
-        public void rellenaComboAutores()
+        
+
+        private void botonActores_Click(object sender, EventArgs e)
+        {
+            String query = "SELECT * FROM actors ORDER BY first_name";
+
+            limpiaListaConsultas();
+            hazLaConsulta(query);
+        }
+
+        private void botonDirectores_Click(object sender, EventArgs e)
+        {
+            String query = "SELECT * FROM directors ORDER BY first_name";
+
+            limpiaListaConsultas();
+            hazLaConsulta(query);
+        }
+
+        
+
+        private void botonPeliculas_Click(object sender, EventArgs e)
+        {
+            String query = "SELECT * FROM movies ORDER BY name";
+
+            limpiaListaConsultas();
+            hazLaConsulta(query);
+            
+        }
+        //El siguiente método, establece la conexión y realiza la consulta sobre la BD
+        private void hazLaConsulta(String query)    
         {
             MySqlConnection conexion = new ConexionBDDPelis().conecta();
-            MySqlCommand comando = new MySqlCommand("SELECT * FROM actors ORDER BY first_name", conexion);
+            MySqlCommand comando = new MySqlCommand(query, conexion);
             MySqlDataReader resultado = comando.ExecuteReader();
-            while (resultado.Read())
-            {
-                String id = resultado.GetString(0);
-                String first_name = resultado.GetString("first_name");
-                String last_name = resultado.GetString("last_name");
-                String gender = resultado.GetString("gender");
-                desplegableCine.Items.Add(id + "|" + first_name + "|" + last_name + "|" + gender);
-            }
-            conexion.Close();
+            datos.Load(resultado);
+            listaConsultas.DataSource = datos;
+
         }
+        //Este método, EN TEORIA, debería limpiar el griefview que muestra las consultas, pero no lo hace :D
+        private void limpiaListaConsultas()
+        {
+            listaConsultas.DataSource = null;
+            listaConsultas.Rows.Clear();
+            listaConsultas.Refresh();
+        }
+
     }
 }
