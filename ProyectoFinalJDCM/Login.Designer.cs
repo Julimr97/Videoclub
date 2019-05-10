e5612488c222ff3ab43aaa3c4995994e9e9ef87a
xp <xp@CETYSDAM006>
2019-05-10 11:40:14 +0200

    Login perfeccionado : Añadido Logo, Background, diseño de letras ...

--- ProyectoFinalJDCM/Login.Designer.cs
+++ ProyectoFinalJDCM/Login.Designer.cs
@@ -28,48 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.lblUsuario = new System.Windows.Forms.Label();
             this.lblPwd = new System.Windows.Forms.Label();
             this.txbUsuario = new System.Windows.Forms.TextBox();
             this.txbPwd = new System.Windows.Forms.TextBox();
             this.btAcceder = new System.Windows.Forms.Button();
+            this.Logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).BeginInit();
             this.SuspendLayout();
             // 
             // lblUsuario
             // 
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(136, 110);
+            this.lblUsuario.BackColor = System.Drawing.Color.Transparent;
+            this.lblUsuario.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsuario.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblUsuario.Location = new System.Drawing.Point(31, 227);
             this.lblUsuario.Name = "lblUsuario";
-            this.lblUsuario.Size = new System.Drawing.Size(46, 13);
+            this.lblUsuario.Size = new System.Drawing.Size(63, 20);
             this.lblUsuario.TabIndex = 0;
             this.lblUsuario.Text = "Usuario:";
             // 
             // lblPwd
             // 
             this.lblPwd.AutoSize = true;
-            this.lblPwd.Location = new System.Drawing.Point(118, 137);
+            this.lblPwd.BackColor = System.Drawing.Color.Transparent;
+            this.lblPwd.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPwd.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblPwd.Location = new System.Drawing.Point(6, 254);
             this.lblPwd.Name = "lblPwd";
-            this.lblPwd.Size = new System.Drawing.Size(64, 13);
+            this.lblPwd.Size = new System.Drawing.Size(88, 20);
             this.lblPwd.TabIndex = 1;
             this.lblPwd.Text = "Contraseña:";
             // 
             // txbUsuario
             // 
-            this.txbUsuario.Location = new System.Drawing.Point(187, 107);
+            this.txbUsuario.Location = new System.Drawing.Point(100, 227);
             this.txbUsuario.Name = "txbUsuario";
             this.txbUsuario.Size = new System.Drawing.Size(267, 20);
             this.txbUsuario.TabIndex = 2;
             // 
             // txbPwd
             // 
-            this.txbPwd.Location = new System.Drawing.Point(187, 134);
+            this.txbPwd.Location = new System.Drawing.Point(100, 254);
             this.txbPwd.Name = "txbPwd";
             this.txbPwd.Size = new System.Drawing.Size(267, 20);
             this.txbPwd.TabIndex = 3;
             // 
             // btAcceder
             // 
-            this.btAcceder.Location = new System.Drawing.Point(258, 193);
+            this.btAcceder.Location = new System.Drawing.Point(171, 313);
             this.btAcceder.Name = "btAcceder";
             this.btAcceder.Size = new System.Drawing.Size(75, 23);
             this.btAcceder.TabIndex = 4;
@@ -77,11 +86,24 @@
             this.btAcceder.UseVisualStyleBackColor = true;
             this.btAcceder.Click += new System.EventHandler(this.btAcceder_Click);
             // 
+            // Logo
+            // 
+            this.Logo.BackColor = System.Drawing.Color.Transparent;
+            this.Logo.Image = ((System.Drawing.Image)(resources.GetObject("Logo.Image")));
+            this.Logo.Location = new System.Drawing.Point(70, 12);
+            this.Logo.Name = "Logo";
+            this.Logo.Size = new System.Drawing.Size(309, 200);
+            this.Logo.TabIndex = 5;
+            this.Logo.TabStop = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(628, 307);
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(446, 350);
+            this.Controls.Add(this.Logo);
             this.Controls.Add(this.btAcceder);
             this.Controls.Add(this.txbPwd);
             this.Controls.Add(this.txbUsuario);
@@ -89,6 +111,7 @@
             this.Controls.Add(this.lblUsuario);
             this.Name = "Login";
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.Logo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -101,5 +124,6 @@
         private System.Windows.Forms.TextBox txbUsuario;
         private System.Windows.Forms.TextBox txbPwd;
         private System.Windows.Forms.Button btAcceder;
+        private System.Windows.Forms.PictureBox Logo;
     }
 }
