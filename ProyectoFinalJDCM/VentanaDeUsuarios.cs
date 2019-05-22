953d536fdafe123bc4819b364a60888bf8e7bc9d
xp <xp@CETYSDAM006>
2019-05-22 11:32:55 +0200

    Revert "añado MessageBox al pulsar REGISTRAR"
    This reverts commit 787bc68a2770e03404f3432d8c8d80ff5b3783ef.

--- ProyectoFinalJDCM/VentanaDeUsuarios.cs
+++ ProyectoFinalJDCM/VentanaDeUsuarios.cs
@@ -28,7 +28,6 @@ namespace ProyectoFinalJDCM
                 "values ('"+ textBoxDNI.Text +"', '"+ textBoxNombre.Text + "', '" + textBoxApellidos.Text + "', '" + textBoxEmail.Text + "', '" + textBoxPSW.Text + "');", conexion);
 
             MySqlDataReader resultado = comando.ExecuteReader();
-            MessageBox.Show("El cliente ha sido registrado correctamente", "BIENVENIDO");
         }
 
         protected override void OnFormClosed(FormClosedEventArgs e)
